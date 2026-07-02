884c19760ef88a4a985b60b61a59658653205fcd
Juan Martinez <juanmartinezcastillo9sena@gmail.com>
2026-07-02 15:07:25 -0500

    Palabra reservada

--- clases/persona.php
+++ clases/persona.php
@@ -1,9 +1,14 @@
 <?php
 class persona
 {
+    public $nombre;
+    public $edad;
+    public $correo;
+    public $apellido;
+
     public function saludar()
     {
-        echo "Hola mi nombre es:  ";
+        echo "Hola, mi nombre es:  ".$this->nombre." tengo ".$this->edad." y mi correo es ".$this->correo."<br>";
         }
 }
 ?>
--- public/index.php
+++ public/index.php
@@ -2,8 +2,20 @@
 
 require_once "../clases/persona.php";
 
-    $juan=new persona();
+    $persona1=new persona();
 
-    $juan -> saludar();
+    $persona1->nombre = "Juan";
+    $persona1->edad = 28;
+    $persona1->correo = "juanmartinezcastillo9@gmail.com";
+    
+    $persona1->saludar() ;
+
+    $persona2=new persona();
+
+    $persona2->nombre = "Pino";
+    $persona2->edad = "21";
+    $persona2->correo = "pino@gmail.com";
+
+    $persona2->saludar();
 
 ?>
